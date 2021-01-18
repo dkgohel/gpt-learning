dc5dec4f328b3a719fa0bd3c5376f4bc901ec677
dkgohel <darshan.gohel619@gmail.com>
2021-01-18 09:18:06 +0300

    Some improvement

--- deepClone.js
+++ deepClone.js
@@ -1,21 +1,31 @@
-function deepCloning(obj)
+function deepCloning(obj,temp)
 {
-    //Null checking
     if(obj===null || typeof obj !== 'object') 
     {
         return obj;
     }
-    var temp = null;
-    if(Array.isArray(obj))
-    {
-        temp=[];
-    }
-    else{
-        temp={};
-    }
+    if (obj.constructor != Object && obj.constructor != Array) return obj;
+    
+    if (obj.constructor == Date || obj.constructor == RegExp || obj.constructor == Function ||
+        obj.constructor == String || obj.constructor == Number || obj.constructor == Boolean)
+        return new obj.constructor(obj);
+    temp = new obj.constructor();
+    //var temp={};
+    // if(Array.isArray(obj))
+    // {
+    //     temp=[];
+    // }
+    // else{
+    //     temp={};
+    // }
+    // for(var x in obj)
+    // {
+    //     temp[x] = deepCloning(obj[x]);
+    // }
+
     for(var x in obj)
     {
-        temp[x] = deepCloning(obj[x]);
+        temp[x] = (typeof temp[x] == 'undefined') ? deepCloning(obj[x], null) : temp[x];
     }
 
     return temp;
@@ -24,7 +34,9 @@ function deepCloning(obj)
 var originalObject = [25,'darArray',{
     name: 'Darshan',
     age: 21,
-    Profession: 'Software Engineer'
+    Profession: 'Software Engineer',
+    list: ["one","two","three"],
+    fun: function(test){ return 1+test }
 }];
 
 var duplicateObject = deepCloning(originalObject);
